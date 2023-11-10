707a11c3fa32f2a74417b86c0d5a5c6c638d9d55
Jesse de Wit <witdejesse@hotmail.com>
2023-11-10 21:26:19 +0100

    format react with linter

--- snippets/react-native/static_channel_backup.ts
+++ snippets/react-native/static_channel_backup.ts
@@ -1,7 +1,7 @@
-import { staticBackup } from "@breeztech/react-native-breez-sdk"
+import { staticBackup } from '@breeztech/react-native-breez-sdk'
 
 const exampleStaticBackup = async () => {
-    // ANCHOR: static-channel-backup
-    let backupData = await staticBackup({ workingDir: "<working directory>" })
-    // ANCHOR_END: static-channel-backup
+  // ANCHOR: static-channel-backup
+  const backupData = await staticBackup({ workingDir: '<working directory>' })
+  // ANCHOR_END: static-channel-backup
 }
