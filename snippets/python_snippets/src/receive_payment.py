c362dc197bfb9bab47ba044b801473ae4c96b58e
ok300 <106775972+ok300@users.noreply.github.com>
2023-11-13 12:13:05 +0100

    Fix indentation

--- snippets/python_snippets/src/receive_payment.py
+++ snippets/python_snippets/src/receive_payment.py
@@ -5,8 +5,8 @@ def receive_payment(sdk_services):
     try:
         # ANCHOR: receive-payment
         req = breez_sdk.ReceivePaymentRequest(
-        amount_msat=3000000,
-        description="Invoice for 3 000 000 sats")
+            amount_msat=3000000,
+            description="Invoice for 3 000 000 sats")
         result = sdk_services.receive_payment(req)
         # ANCHOR_END: receive-payment
         return result
