5f541d48033d3647dfb09db71e701a27bbafdc5f
Roei Erez <roeierez@gmail.com>
2023-12-14 10:13:04 +0200

    fix swift snippet

--- snippets/swift/BreezSDKExamples/Sources/Webhook.swift
+++ snippets/swift/BreezSDKExamples/Sources/Webhook.swift
@@ -15,6 +15,6 @@ func webhook(sdk: BlockingBreezServices) throws {
 
 func paymentWebhook(sdk: BlockingBreezServices) {
     // ANCHOR: register-payment-webook
-    sdk.registerWebhook(webhookUrl: "https://your-nds-service.com/notify?platform=ios&token=<PUSH_TOKEN>").await?    
+    sdk.registerWebhook(webhookUrl: "https://your-nds-service.com/notify?platform=ios&token=<PUSH_TOKEN>")  
     // ANCHOR_END: register-payment-webook
 }
