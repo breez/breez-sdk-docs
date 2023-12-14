c5b43c1e002b7ab9810b51abd936c31ac315e746
Roei Erez <roeierez@gmail.com>
2023-12-14 11:47:40 +0200

    fix rust code

--- snippets/rust/src/webhook.rs
+++ snippets/rust/src/webhook.rs
@@ -4,7 +4,7 @@ use breez_sdk_core::*;
 
 async fn webhook(sdk: Arc<BreezServices>) -> Result<()> {
     // ANCHOR: register-webook
-    sdk.register_webhook("https://yourapplication.com").await?
+    sdk.register_webhook("https://yourapplication.com").await?;
     // ANCHOR_END: register-webook
 
     Ok(())
@@ -12,7 +12,7 @@ async fn webhook(sdk: Arc<BreezServices>) -> Result<()> {
 
 async fn payment_webhook(sdk: Arc<BreezServices>) -> Result<()> {
   // ANCHOR: register-payment-webook
-  sdk.register_webhook("https://your-nds-service.com/notify?platform=ios&token=<PUSH_TOKEN>").await?    
+  sdk.register_webhook("https://your-nds-service.com/notify?platform=ios&token=<PUSH_TOKEN>").await?;    
   // ANCHOR_END: register-payment-webook
 
   Ok(())
