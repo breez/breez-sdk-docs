69ebd18b726e93e5e827758381898b78d9b4cd56
ok300 <106775972+ok300@users.noreply.github.com>
2024-01-20 17:43:24 +0100

    Bump the Swift snippet macos version to v13

--- snippets/swift/BreezSDKExamples/Package.swift
+++ snippets/swift/BreezSDKExamples/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "BreezSDKDocs",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v13)],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
         .package(url: "https://github.com/breez/breez-sdk-swift", from:"0.2.12")
