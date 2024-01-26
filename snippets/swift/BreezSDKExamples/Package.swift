20576b6edebfaf51672a33c949786c2d8f2db10f
ruben beck <ruben.beck@pm.me>
2024-01-26 14:29:16 +0100

    example on how to use local breez-sdk package

--- snippets/swift/BreezSDKExamples/Package.swift
+++ snippets/swift/BreezSDKExamples/Package.swift
@@ -7,8 +7,9 @@ let package = Package(
     name: "BreezSDKDocs",
     platforms: [.macOS(.v13)],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
         .package(url: "https://github.com/breez/breez-sdk-swift", from:"0.2.14")
+        // use a local version of breezsdk
+        // .package(path: "<YOUR_PATH_TO_BREEZ_SDK>/libs/sdk-bidning/bindings-swift/")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -16,8 +17,9 @@ let package = Package(
         .executableTarget(
             name: "BreezSDKDocs",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "BreezSDK", package: "breez-sdk-swift"),
+                // use a local version of breez-sdk
+                // .product(name: "BreezSDK", package: "bindings-swift"),
             ],
             path: "Sources"),
     ]
