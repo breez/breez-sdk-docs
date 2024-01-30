c7f078cc06f3cf0aeda2551351f338863b0514f2
ok300 <106775972+ok300@users.noreply.github.com>
2024-01-30 17:01:49 +0100

    Fix dart issues

--- snippets/dart_snippets/lib/communicating_fees.dart
+++ snippets/dart_snippets/lib/communicating_fees.dart
@@ -3,7 +3,7 @@ import 'package:breez_sdk/bridge_generated.dart';
 
 Future<void> getFeeInfoAfterInvoiceCreated({required ReceivePaymentResponse receivePaymentResponse}) async {
   // ANCHOR: get-fee-info-after-invoice-created
-  int openingFeeSat = (receivePaymentResponse.openingFeeMsat ?? 0) / 1000;
+  int openingFeeSat = (receivePaymentResponse.openingFeeMsat ?? 0) / 1000 as int;
   print("A setup fee of ${openingFeeSat} sats is applied to this invoice.");
   // ANCHOR_END: get-fee-info-after-invoice-created
 }
