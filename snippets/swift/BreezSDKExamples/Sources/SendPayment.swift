b62e29dceb389c278cc425074159180b234e29e4
Ross Savage <551697+dangeross@users.noreply.github.com>
2024-04-13 07:49:49 +0200

    Update swift syntax

--- snippets/swift/BreezSDKExamples/Sources/SendPayment.swift
+++ snippets/swift/BreezSDKExamples/Sources/SendPayment.swift
@@ -11,7 +11,7 @@ func sendPayment(sdk: BlockingBreezServices) -> SendPaymentResponse? {
     // ANCHOR: send-payment
     // The `amountMsat` param is optional and should only passed if the bolt11 doesn't specify an amount.
     // The amountMsat is required in case an amount is not specified in the bolt11 invoice'.
-    let optionalAmountMsat = 3_000_000
+    let optionalAmountMsat: UInt64 = 3_000_000
     let optionalLabel = "<label>"
     let req = SendPaymentRequest(bolt11: "...", amountMsat: optionalAmountMsat, label: optionalLabel)
     let response = try? sdk.sendPayment(req: req)
