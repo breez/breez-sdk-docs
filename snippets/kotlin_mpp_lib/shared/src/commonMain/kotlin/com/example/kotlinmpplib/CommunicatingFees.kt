1add2ae508892fb6ae7250b1a5857cb4142d9af8
ok300 <106775972+ok300@users.noreply.github.com>
2024-04-17 18:31:26 +0200

    Fix kotlin CI error

--- snippets/kotlin_mpp_lib/shared/src/commonMain/kotlin/com/example/kotlinmpplib/CommunicatingFees.kt
+++ snippets/kotlin_mpp_lib/shared/src/commonMain/kotlin/com/example/kotlinmpplib/CommunicatingFees.kt
@@ -5,7 +5,7 @@ class CommunicatingFees {
     fun get_fee_info_after_invoice_created(receivePaymentResponse: ReceivePaymentResponse) {
         // ANCHOR: get-fee-info-after-invoice-created
         val openingFeeSat = (receivePaymentResponse.openingFeeMsat ?: 0).toULong() / 1000;
-        Log.v("Breez", "A setup fee of ${openingFeeSat} sats is applied to this invoice.")
+        // Log.v("Breez", "A setup fee of ${openingFeeSat} sats is applied to this invoice.")
         // ANCHOR_END: get-fee-info-after-invoice-created
     }
 }
