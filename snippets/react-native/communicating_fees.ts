e95fe7b7c4ecf23c3c1fadb16efa24f36cf0fa88
ok300 <106775972+ok300@users.noreply.github.com>
2024-04-17 19:19:38 +0200

    Fix RN CI errors

--- snippets/react-native/communicating_fees.ts
+++ snippets/react-native/communicating_fees.ts
@@ -1,12 +1,12 @@
 import {
-    receivePayment,
-    ReceivePaymentResponse
+  receivePayment,
+  type ReceivePaymentResponse
 } from '@breeztech/react-native-breez-sdk'
 
-const get_fee_info_after_invoice_created = async (receivePaymentResponse: ReceivePaymentResponse) => {
+const getFeeInfoAfterInvoiceCreated = async (receivePaymentResponse: ReceivePaymentResponse) => {
   // ANCHOR: get-fee-info-after-invoice-created
   const openingFeeMsat = receivePaymentResponse.openingFeeMsat
-  const openingFeeSat = openingFeeMsat ? openingFeeMsat / 1000 : 0
+  const openingFeeSat = openingFeeMsat ?? openingFeeMsat / 1000 : 0
   console.log(`A setup fee of ${openingFeeSat} sats is applied to this invoice.`)
   // ANCHOR_END: get-fee-info-after-invoice-created
 }
