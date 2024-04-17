d8d496ecfc139b5eeac2acff62b881af3e680853
ok300 <106775972+ok300@users.noreply.github.com>
2024-04-17 18:36:51 +0200

    Fix dart CI errors

--- snippets/dart_snippets/lib/communicating_fees.dart
+++ snippets/dart_snippets/lib/communicating_fees.dart
@@ -1,9 +1,8 @@
-import 'package:breez_sdk/breez_sdk.dart';
 import 'package:breez_sdk/bridge_generated.dart';
 
 Future<void> getFeeInfoAfterInvoiceCreated({required ReceivePaymentResponse receivePaymentResponse}) async {
   // ANCHOR: get-fee-info-after-invoice-created
   int openingFeeSat = (receivePaymentResponse.openingFeeMsat ?? 0) / 1000 as int;
-  print("A setup fee of ${openingFeeSat} sats is applied to this invoice.");
+  print("A setup fee of $openingFeeSat sats is applied to this invoice.");
   // ANCHOR_END: get-fee-info-after-invoice-created
 }
