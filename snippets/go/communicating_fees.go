f4f90dc706286a9a6c50ef5c23971199c5f349e4
ok300 <106775972+ok300@users.noreply.github.com>
2024-04-17 18:37:34 +0200

    Fix golang CI error

--- snippets/go/communicating_fees.go
+++ snippets/go/communicating_fees.go
@@ -13,7 +13,7 @@ func GetFeeInfoAfterInvoiceCreated(receivePaymentResponse breez_sdk.ReceivePayme
 	if openingFeeMsat != nil {
 		openingFeeSat = *openingFeeMsat / 1000
 	} else {
-	    openingFeeSat = 0
+		openingFeeSat = 0
 	}
 	log.Printf("A setup fee of %#v sats is applied to this invoice.", openingFeeSat)
 	// ANCHOR_END: get-fee-info-after-invoice-created
