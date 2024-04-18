c9c2091c0b362a5eb4feabbdbdf44707ebf3282f
ok300 <106775972+ok300@users.noreply.github.com>
2024-04-18 07:56:41 +0200

    Fix kotlin CI error

--- snippets/kotlin_mpp_lib/shared/src/commonMain/kotlin/com/example/kotlinmpplib/CommunicatingFees.kt
+++ snippets/kotlin_mpp_lib/shared/src/commonMain/kotlin/com/example/kotlinmpplib/CommunicatingFees.kt
@@ -4,7 +4,7 @@ import breez_sdk.*
 class CommunicatingFees {
     fun get_fee_info_after_invoice_created(receivePaymentResponse: ReceivePaymentResponse) {
         // ANCHOR: get-fee-info-after-invoice-created
-        val openingFeeSat = receivePaymentResponse.openingFeeMsat?.let{ it.div(1000L).toULong() } ?: run { 0UL }
+        val openingFeeSat = receivePaymentResponse.openingFeeMsat?.let{ it.div(1000UL).toULong() } ?: run { 0UL }
         // Log.v("Breez", "A setup fee of ${openingFeeSat} sats is applied to this invoice.")
         // ANCHOR_END: get-fee-info-after-invoice-created
     }
